25f5fe06253cbd29927a25aa74bb8660d8dc97ee
Marshall Couch <marshall.couch@gmail.com>
2026-07-23 11:10:05 -0400

    tab targeting!

--- src/main.gd
+++ src/main.gd
@@ -8,25 +8,55 @@ func _ready():
 	actions = $ActionBar.get_actions()
 	for a:Action in actions:
 		a.action_attempt.connect(perform_action)
+		
+func get_characters() -> Array[Character]:
+	var characters:Array[Character] =[]
+	for a:Character in $Allies.get_children():
+		characters.append(a)
+	for e:Character in $Enemies.get_children():
+		characters.append(e)
+	return characters
 
 func _input(event: InputEvent) -> void:
 	if mouse_over_actions:
 		return
+
 	var selected_char = get_selected_character()
+	#mouse selection handling
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and selected_char != null:
-		for e:Character in $Enemies.get_children():
-			if e.selected and !e.mouse_over_me:
-				e.selected = false
-		for a:Character in $Allies.get_children():
-			if a.selected and !a.mouse_over_me:
-				a.selected = false
+		for c:Character in get_characters():
+			if c.selected and !c.mouse_over_me:
+				c.selected = false
+	#tab_targeting.
+	if event.is_action_released("tab"):
+		if selected_char == null:
+			var a:Character = $Allies.get_child(0)
+			a.selected = true
+			return
+		if Input.is_key_pressed(KEY_SHIFT):
+			var tabbable_targets = get_characters()
+			var selected_index:int = tabbable_targets.find(selected_char)
+			if selected_index == 0:
+				tabbable_targets[tabbable_targets.size()-1].selected = true
+			else:
+				tabbable_targets[selected_index-1].selected = true
+			selected_char.selected = false
+		else: #shift isn't held and we should normal tab
+			var tabbable_targets = get_characters()
+			var selected_index:int = tabbable_targets.find(selected_char)
+			if selected_index == tabbable_targets.size()-1:
+				tabbable_targets[0].selected = true
+			else:
+				tabbable_targets[selected_index+1].selected = true
+			selected_char.selected = false
+			
+		
+				
+	
 func get_selected_character() -> Character:
-	for e:Character in $Enemies.get_children():
-		if e.selected:
-			return e
-	for a:Character in $Allies.get_children():
-		if a.selected:
-			return a
+	for c:Character in get_characters():
+		if c.selected:
+			return c
 	return null
 
 #function used by all the action buttons.  determine if action is possible
