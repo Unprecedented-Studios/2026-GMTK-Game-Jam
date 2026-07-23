5a1976b7495c7e0cc3d04ba91c7b46de64546ad5
Matt Couch <mcouch@gmail.com>
2026-07-22 19:26:40 -0500

    started creating enemies and health

---
+++ src/Abilities/damageInfo.gd
@@ -0,0 +1,15 @@
+class_name  DamageInfo
+
+enum damage_types {
+	NORMAL,
+	ELECTRIC,
+	AIR,
+	WATER,
+	FIRE,
+	EARTH,
+	ICE,
+	POISON
+}
+
+var damage: float = 1
+var type: damage_types = damage_types.NORMAL;
---
+++ src/Characters/heros/basic.gd
@@ -0,0 +1,8 @@
+extends Character
+class_name basicHero
+
+func _process(_delta: float) -> void:
+	var enemies:Array = get_tree().get_nodes_in_group("enemies");
+	
+	var e = enemies.pick_random();
+	e.take_damage(DamageInfo.new())
