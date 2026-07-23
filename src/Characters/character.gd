7b1440176755ad740fa94b1d89c29d8e84e04bd8
Matt Couch <mcouch@gmail.com>
2026-07-22 21:02:12 -0500

    starting moving to animation player

--- src/Characters/character.gd
+++ src/Characters/character.gd
@@ -7,10 +7,12 @@ var current_hp;
 @onready var health_bar:StatusBox = get_node("Status/HealthBar");
 @onready var buff_display:GridContainer = get_node("Status/BuffsAndDebuffs");
 @onready var sprite:AnimatedSprite2D = get_node("Character");
+@onready var animation:AnimationPlayer = get_node("AnimationPlayer");
 
 func _ready():
 	current_hp = max_hp;
 	sprite.play("idle")
+	sprite.animation_finished.connect(_on_character_animation_finished);
 	pass
 	
 func _update_healthBar():
@@ -27,13 +29,16 @@ func take_damage(info:DamageInfo) -> void:
 	var currDamage = info;
 	#loop through buff list for modifiers
 	
-	current_hp -= currDamage.damage
-	#TODO play damage animation based on type?
-	
-	if current_hp <= 0:
-		die();
-	_update_healthBar();
-	
+	if currDamage.damage > 0:
+		
+		sprite.play("hurt")
+		sprite.frame=0
+		current_hp -= currDamage.damage
+		
+		if current_hp <= 0:
+			die();
+		_update_healthBar();
+		
 func heal(amount:int) -> void:
 	var currHeal = amount;
 	
@@ -52,12 +57,16 @@ func die() -> void:
 	#TODO: death animation?
 	sprite.play("die")
 	
-	sprite.animation_finished.connect(_on_character_animation_finished)
-	
 	_update_healthBar();
 
 
 func _on_character_animation_finished():
 	if current_hp == 0:
 		queue_free();
+	else:
+		sprite.play("idle")
+
 	pass # Replace with function body.
+
+func attack_hit(num:int):
+	pass;
--- src/Characters/heros/basic.gd
+++ src/Characters/heros/basic.gd
@@ -1,8 +1,32 @@
 extends Character
 class_name basicHero
 
-func _process(_delta: float) -> void:
+@export var attack_rate:float = 1;
+@export var damage: float = 1;
+@export var type: DamageInfo.damage_types = DamageInfo.damage_types.NORMAL;
+
+var target:Character;
+
+func _ready():
+	super._ready();
+	
+	var attack_timer = Timer.new()
+	add_child(attack_timer)
+	attack_timer.one_shot = false;
+	attack_timer.start(attack_rate)
+	attack_timer.timeout.connect(_attack);
+	
+
+func _attack() -> void:
 	var enemies:Array = get_tree().get_nodes_in_group("enemies");
 	if enemies.size() > 0:
-		var e = enemies.pick_random();
-		e.take_damage(DamageInfo.new())
+		target = enemies.pick_random();
+		animation.play("attack1");
+		
+func attack_hit(id:int) -> void:
+	if id == 1:
+		var dmg = DamageInfo.new();
+		dmg.damage = damage;
+		dmg.type = type;
+		
+		target.take_damage(dmg)
