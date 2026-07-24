52b1f6e613c495d159cf2a8213e395ab617c87cf
Marshall Couch <marshall.couch@gmail.com>
2026-07-24 13:17:26 -0400

    deleted buff stuff and updatd action display to work properly

--- src/action_display.gd
+++ src/action_display.gd
@@ -10,6 +10,7 @@ func _ready() -> void:
 func set_action_display(_type: Action.actions_list, level:int =0):
 	type = _type
 	$ActionButton.action_type = _type
+	$ActionButton.set_icon()
 	$ActionButton.set_upgrade_level(level)
 	$Label.text = Action.action_information[_type]["info"]
 	
